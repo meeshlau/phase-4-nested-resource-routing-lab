036e7e3b299d21679fdda6d9cbb0b20f5c3f86f0
Michelle Lau <meeshklau@gmail.com>
2022-10-14 13:12:54 -1000

    commit tree 8b07b27a33fb89ca14756ce6197172c7bd222272

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,60 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  # def index 
+  #   binding.pry
+  #   user = User.find_by(id: params[:user_id])
+    
+  #   if user
+  #     render json: user.items
+  #   elsif params[:user_id]
+  #     render json: { error: "User not found"}, status: :not_found
+  #   else
+  #      items = Item.all
+  #      render json: items, include: :user
+  #   end 
+  # end 
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if user = User.find_by(id: params[:user_id])
+      render json: user.items
+    elsif params[:user_id]
+      render json: { error: "User not found" }, status: :not_found
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
   end
+
+  def show
+    if item = Item.find_by(id: params[:id])
+      render json: item
+    else 
+      render_not_found_response
+    end 
+  end
+
+  def create
+    find_user = User.find_by(id: params[:user_id])
+
+    if find_user
+      new_item = Item.create(
+        name: params[:name],
+        description: params[:description],
+        price: params[:price],
+        user_id: find_user.id)
+      render json: new_item, status: :created
+    else
+      render_not_found_response
+    end
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Not found" }, status: :not_found
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index] 
+  
+  resources :users, only: [:index, :show] do 
+    resources :items, only: [:show, :index, :create]
+  end
 end
